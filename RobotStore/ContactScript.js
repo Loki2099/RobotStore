fd47ee142afa0da28055e7143b1c7cb5e52bdd32
Loki2099 <Mark.Satterwhite2099@gmail.com>
2017-11-08 15:48:35 -0600

    Started Scripts

---
+++ RobotStore/ContactScript.js
@@ -0,0 +1,16 @@
+﻿$(function(){LoadContactJS();});
+
+
+function LoadContactJS(){
+	$("#fullname").focusin(function(){ActiveField(this);});
+	$("#fullname").focusout(function(){ActiveField(this);});
+	$("#email").focusin(function(){ActiveField(this);});
+	$("#email").focusout(function(){ActiveField(this);});
+	$("#yloc").focusin(function(){ActiveField(this);});
+	$("#yloc").focusout(function(){ActiveField(this);});
+	$("textarea[name='quest']").focusin(function(){ActiveField(this);});
+	$("textarea[name='quest']").focusout(function(){ActiveField(this);});
+
+	function ActiveField(aip){$(aip).toggleClass("HighLight");}
+}
+
---
+++ RobotStore/RobotScript.js
@@ -0,0 +1,13 @@
+﻿$(function(){LoadRobotJS();});
+
+function LoadRobotJS(){
+	$("#robo1").mouseover(function(){lineOver(this);});
+	$("#robo1").mouseleave(function(){lineOver(this);});
+	$("#robo2").mouseover(function(){lineOver(this);});
+	$("#robo2").mouseleave(function(){lineOver(this);});
+	$("#robo3").mouseover(function(){lineOver(this);});
+	$("#robo3").mouseleave(function(){lineOver(this);});
+
+
+	function lineOver(emt){$(emt).toggleClass("HighLightR");}
+}
